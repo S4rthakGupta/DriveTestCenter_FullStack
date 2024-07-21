4b7ff6a29a2ffe9ecb513f340f5393846d2b5ff1
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-07-21 17:18:04 -0400

    Updated Schema (Models-appointment.js)

--- models/appointment.js
+++ models/appointment.js
@@ -1,9 +1,14 @@
+// Importing the mongoose library, which is used to interact with MongoDB.
 const mongoose = require("mongoose");
 
-const appointmentSchema = new mongoose.Schema({
-  date: { type: String, required: true }, // Use String to match the date format in the example
+// Defining a new schema for an appointment with various fields and their types/requirements.
+const appointmentSchema = new mongoose.Schema(
+{ 
+  // Defining a new schema for the usertype = admin to add the apppointments in the application.
+  date: { type: String, required: true },
   time: { type: String, required: true },
   isTimeSlotAvailable: { type: Boolean, required: true, default: true }
 });
 
+// This below line is exporting the Appointment model based on the defined schema.
 module.exports = mongoose.model("Appointment", appointmentSchema);
