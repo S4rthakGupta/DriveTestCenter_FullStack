11e3b009dff1982c73de53e94c116fe11bb25031
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-07-21 17:40:46 -0400

    upadting bookAppointment function

--- controllers/userController.js
+++ controllers/userController.js
@@ -182,31 +182,39 @@ const appointmentPage = (req, res) =>
 
 // Controller function for booking an appointment slot for Drivers.
 const bookAppointment = (req, res) => 
-{
+{  
+  // This below line will check if the user is authenticated and is a Driver.
   if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') 
   {
     const userId = res.locals.user._id;
     const { appointmentId } = req.body;
 
+    // It will find the user by ID.
     User.findById(userId)
       .then(user => 
       {
+        // This below if statement will check if user has completed their personal information or not.
         if (!user.firstName || !user.lastName || !user.licenseNumber || !user.age) {
           return res.status(403).send('Please complete your personal information before booking an appointment.');
         }
-
+        
+        // This will update the appointment slot to mark it as booked.
         Appointment.findByIdAndUpdate(appointmentId, { isTimeSlotAvailable: false }, { new: true })
           .then(updatedAppointment => 
           {
             if (!updatedAppointment) return res.status(404).send('Appointment not found');
             console.log('Updated appointment data:', updatedAppointment);
 
+            
+            // This will update the user's record with the booked appointment ID
             User.findByIdAndUpdate(userId, { appointment: appointmentId }, { new: true })
               .then(updatedUser => 
               {
                 if (!updatedUser) return res.status(404).send('User not found');
                 console.log('Updated user data:', updatedUser);
+                // This will set a success message in the session.
                 req.session.message = 'Appointment booked successfully';
+                // After that it will redirect the user to the G page.
                 res.redirect('/g');
               })
               .catch(err => 
@@ -229,6 +237,7 @@ const bookAppointment = (req, res) =>
   } 
   else 
   {
+    // It will redirect the user to login page again if the userType is not Driver.
     res.redirect('/login');
   }
 };
