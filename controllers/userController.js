8162c6eecfe658d08ad729ef4df242ce1a69a581
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-07-21 17:29:17 -0400

    adding comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,39 @@
+// Requiring the User Schema from the models folder.
 const User = require('../models/user');
+
+// Importing necessary models and libraries.
 const Appointment = require('../models/appointment');
 const moment = require('moment');
 
-// Render dashboard page.
-const dashboard = (req, res) => {
-  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') {
+// Controller function for rendering the dashboard page.
+const dashboard = (req, res) => 
+{   
+  //This below line will check if user is authenticated and is of type 'Driver'.
+  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') 
+  {
     res.render('pages/dashboard', { title: 'Dashboard' });
-  } else {
+  }   
+  // This else block will redirect to login page if not authenticated or not a driver
+  else 
+  {
     res.redirect('/login');
   }
 };
 
 // Render G2 page.
-const g2Page = (req, res) => {
-  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') {
+const g2Page = (req, res) => 
+{
+  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') 
+  {
     const userId = res.locals.user._id;
     const selectedDate = req.query.date || moment().format('YYYY-MM-DD'); // default to today
 
     User.findById(userId)
-      .then(user => {
+      .then(user => 
+      {
         Appointment.find({ date: selectedDate, isTimeSlotAvailable: true })
-          .then(appointments => {
+          .then(appointments => 
+          {
             const message = req.session.message;
             delete req.session.message;
 
@@ -33,7 +46,8 @@ const g2Page = (req, res) => {
             const hasPersonalInfo = user.firstName && user.lastName && user.licenseNumber && user.age;
 
             console.log('User object for G2 page:', user);
-            res.render('pages/g2', { 
+            res.render('pages/g2', 
+            { 
               title: 'G2 Page', 
               user, 
               appointments, 
@@ -43,27 +57,34 @@ const g2Page = (req, res) => {
               hasPersonalInfo  // Pass this flag to the view
             });
           })
-          .catch(err => {
+          .catch(err => 
+          {
             console.log('Error fetching appointments:', err);
             res.status(500).send('Error fetching appointments');
           });
       })
-      .catch(err => {
+      .catch(err => 
+      {
         console.log('Error fetching user data:', err);
         res.status(500).send('Error fetching user data');
       });
-  } else {
+  } 
+  else
+  {
     res.redirect('/login');
   }
 };
 
 // Render G page.
-const gPage = (req, res) => {
-  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') {
+const gPage = (req, res) => 
+{
+  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') 
+  {
     const userId = res.locals.user._id;
 
     User.findById(userId).populate('appointment')
-      .then(user => {
+      .then(user => 
+      {
         console.log('Fetched user data:', user); // Log user data
 
         const message = req.session.message;
@@ -72,30 +93,37 @@ const gPage = (req, res) => {
         // Check if the user has provided all personal information
         const hasPersonalInfo = user.firstName && user.lastName && user.licenseNumber && user.age;
 
-        res.render('pages/g', { 
+        res.render('pages/g', 
+        { 
           title: 'G Page', 
           user, 
           message, 
           hasPersonalInfo 
         });
       })
-      .catch(err => {
+      .catch(err => 
+      {
         console.log('Error fetching user data:', err);
         res.status(500).send('Error fetching user data');
       });
-  } else {
+  } 
+  else 
+  {
     res.redirect('/login');
   }
 };
 
 // Save user data.
-const saveUserData = (req, res) => {
-  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') {
+const saveUserData = (req, res) => 
+{
+  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') 
+  {
     console.log('Request body:', req.body); // Log the request body
     const userId = res.locals.user._id;
     const { firstName, lastName, licenseNumber, age, carMake, carModel, carYear, plateNumber, appointmentId } = req.body;
 
-    const updateData = {
+    const updateData = 
+    {
       firstName,
       lastName,
       licenseNumber,
@@ -107,91 +135,113 @@ const saveUserData = (req, res) => {
     };
 
     // Include appointmentId if it exists
-    if (appointmentId) {
+    if (appointmentId) 
+    {
       updateData.appointment = appointmentId;
     }
 
     User.findByIdAndUpdate(userId, updateData, { new: true })
-      .then(updatedUser => {
+      .then(updatedUser => 
+      {
         if (!updatedUser) return res.status(404).send('User not found');
         console.log('Updated user data:', updatedUser);
         req.session.message = 'User data updated successfully';
         res.redirect('/g');
       })
-      .catch(err => {
+      .catch(err =>
+      {
         console.log('Error updating user data:', err);
         res.status(500).send('Error updating user data');
       });
-  } else {
+  } 
+  else 
+  {
     res.redirect('/login');
   }
 };
 
 // Render appointment page for Admin.
-const appointmentPage = (req, res) => {
+const appointmentPage = (req, res) => 
+{
   if (res.locals.isAuthenticated && res.locals.user.userType === 'Admin') {
     const message = req.session.message; // Fetch message from session if needed
     res.render('pages/appointment', { title: 'Appointment', message }); // Pass message to the template
-  } else {
+  } 
+  else 
+  {
     res.redirect('/login');
   }
 };
 
 // Add appointment slots.
-const bookAppointment = (req, res) => {
-  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') {
+const bookAppointment = (req, res) => 
+{
+  if (res.locals.isAuthenticated && res.locals.user.userType === 'Driver') 
+  {
     const userId = res.locals.user._id;
     const { appointmentId } = req.body;
 
     User.findById(userId)
-      .then(user => {
+      .then(user => 
+      {
         if (!user.firstName || !user.lastName || !user.licenseNumber || !user.age) {
           return res.status(403).send('Please complete your personal information before booking an appointment.');
         }
 
         Appointment.findByIdAndUpdate(appointmentId, { isTimeSlotAvailable: false }, { new: true })
-          .then(updatedAppointment => {
+          .then(updatedAppointment => 
+          {
             if (!updatedAppointment) return res.status(404).send('Appointment not found');
             console.log('Updated appointment data:', updatedAppointment);
 
             User.findByIdAndUpdate(userId, { appointment: appointmentId }, { new: true })
-              .then(updatedUser => {
+              .then(updatedUser => 
+              {
                 if (!updatedUser) return res.status(404).send('User not found');
                 console.log('Updated user data:', updatedUser);
                 req.session.message = 'Appointment booked successfully';
                 res.redirect('/g');
               })
-              .catch(err => {
+              .catch(err => 
+              {
                 console.log('Error updating user data:', err);
                 res.status(500).send('Error updating user data');
               });
           })
-          .catch(err => {
+          .catch(err => 
+          {
             console.log('Error updating appointment:', err);
             res.status(500).send('Error updating appointment');
           });
       })
-      .catch(err => {
+      .catch(err => 
+      {
         console.log('Error fetching user data:', err);
         res.status(500).send('Error fetching user data');
       });
-  } else {
+  } 
+  else 
+  {
     res.redirect('/login');
   }
 };
 
-const addAppointment = (req, res) => {
-  if (res.locals.isAuthenticated && res.locals.user.userType === 'Admin') {
+const addAppointment = (req, res) => 
+{
+  if (res.locals.isAuthenticated && res.locals.user.userType === 'Admin') 
+  {
     const { date, times } = req.body;
 
     console.log('Received appointment data:', { date, times });
 
-    if (!date || !times) {
+    if (!date || !times) 
+    {
       console.log('Missing date or times in request body');
       return res.status(400).send('Missing date or times in request body');
     }
 
-    const appointments = Array.isArray(times) ? times.map(time => ({
+    const appointments = Array.isArray(times) ? times.map(time => (
+    {
       date,
       time,
       isTimeSlotAvailable: true
@@ -201,49 +251,61 @@ const addAppointment = (req, res) => {
 
     const timeSlots = Array.isArray(times) ? times : [times];
     Appointment.find({ date, time: { $in: timeSlots } })
-      .then(existingAppointments => {
+      .then(existingAppointments => 
+      {
         if (existingAppointments.length > 0) {
           req.session.message = 'Some time slots are already booked';
           return res.redirect('/appointment');
-        } else {
+        } 
+        else 
+        {
           Appointment.insertMany(appointments)
-            .then(insertedAppointments => {
+            .then(insertedAppointments => 
+            {
               console.log('Appointments added successfully:', insertedAppointments);
               req.session.message = 'Appointments added successfully';
               res.redirect('/appointment');
             })
-            .catch(err => {
+            .catch(err => 
+            {
               console.log('Error adding appointments:', err);
               res.status(500).send('Error adding appointments');
             });
         }
       })
-      .catch(err => {
+      .catch(err => 
+      {
         console.log('Error checking existing appointments:', err);
         res.status(500).send('Error checking existing appointments');
       });
-  } else {
+  } 
+  else 
+  {
     res.redirect('/login');
   }
 };
 
 // Fetch booked time slots for a date.
-const getBookedTimesForDate = (req, res) => {
+const getBookedTimesForDate = (req, res) =>
+{
   const date = req.params.date;
 
   Appointment.find({ date, isTimeSlotAvailable: false })
-    .then(appointments => {
+    .then(appointments => 
+    {
       const bookedTimes = appointments.map(app => app.time);
       console.log('Booked times for date', date, ':', bookedTimes);
       res.json(bookedTimes);
     })
-    .catch(err => {
+    .catch(err => 
+    {
       console.log('Error fetching appointments:', err);
       res.status(500).send('Error fetching appointments');
     });
 };
 
-module.exports = {
+module.exports = 
+{
   dashboard,
   g2Page,
   gPage,
