91b4a06f181634a3e97efc6670829d98ff5f00e9
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-07-21 17:11:26 -0400

    updating middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,14 @@
-module.exports.isAuthenticated = (req, res, next) => {
-  if (req.session.isAuthenticated) {
+// This below is a middleware function to check if the user is authenticated.
+module.exports.isAuthenticated = (req, res, next) => 
+{
+  
+  // This will check if the session contains an 'isAuthenticated' flag set to true.
+  if (req.session.isAuthenticated) 
+  {
     return next();
   }
+
+  
+  // If the user is not authenticated, it will redirect the user to the login page.
   res.redirect('/login');
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
+// Importing the reuired module.
 const express = require("express");
+
+// Creating a new router setup
 const router = express.Router();
+
+// Requiring the controller from the controllers folder.
 const userController = require("../controllers/userController");
 
-// Import the isAuthenticated middleware
+// Importing the isAuthenticated middleware.
 const { isAuthenticated } = require('../middleware/auth');
 
-// Driver routes
+// Below are the routers for the user's personal information as well as the admin user-type.(first name, last name, vehicle details, adding appointment, booking appointment. etc).
 router.get("/dashboard", userController.dashboard);
 router.get("/g2", userController.g2Page);
 router.get("/g", userController.gPage);
@@ -14,7 +19,8 @@ router.get('/appointment', isAuthenticated, userController.appointmentPage);
 router.post('/add-appointment', isAuthenticated, userController.addAppointment);
 router.post('/book-appointment', isAuthenticated, userController.bookAppointment);
 
+// This is the route to get booked times for a specific date and these are protected by authentication middleware.
 router.get('/appointments/:date', isAuthenticated, userController.getBookedTimesForDate);
 
-
+// Exporting the routers so that they can be used in the whole project.
 module.exports = router;
